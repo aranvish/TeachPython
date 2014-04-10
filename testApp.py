7bcff2c07e10d9bb84de6396dfa4403f9a6562ca
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-04-09 20:16:18 -0400

    slighlty better interface, but still not able to show message and take input and still mostly ugly

--- testApp.py
+++ testApp.py
@@ -35,27 +35,23 @@ class TestApp(App):
     '''A test app designed to create a pop up with text instructions and to 
     allow for the input of text which will hopefully be bound so it can be executed'''      
     def build(self):
-        message = 'Have you ever wanted to do the same thing more than once?  \n\n'
-        message2 = 'Like a lot of times? \n\n'
-        message3 = 'Did you know if you use a for loop, you can do the same thing as many times as you want without re-typing the same code? \n\n'
-        message4 = 'Pretty cool, huh\n\n\n'
-        return Button(on_press=partial(self.popup_display, "Did you know...?", message+message2+message3+message4))
+        return Button(on_press=partial(self.popup_display, "Type your code here:"))
         
     def executeText(self, evt=None):
         '''saves user input as a variable to be executed'''
         userInput = self.userInput.text
         exec(userInput)
         
-    def popup_display(self, title, message,  widget):
-        btnenter = Button(text = 'enter', size_hint_y = None, height =50)
-        btnclose = Button(text='Close me', size_hint_y=None, height=50)
+    def popup_display(self, title,  widget):
+        btnenter = Button(text = 'enter', size_hint_y = None, size_hint_x = 0.5, height =50)
+        btnclose = Button(text='Close me', size_hint_y=None, size_hint_x = 0.5, height=50)
         self.userInput = TextInput(size_hint = (1,1))
 
-        layout = BoxLayout(orientation='horizontal', height = 300)
-        layout.add_widget(Label(text = message, size_hint = (1, 1)))
+        layout = BoxLayout(orientation='vertical', height = 300)
+#        layout.add_widget(Label(text = message, size_hint = (1, 1)))
         layout.add_widget(self.userInput)
-        layout.add_widget(btnclose)
         layout.add_widget(btnenter)
+        layout.add_widget(btnclose)
         sv = ScrollView(size_hint=(None, None), size=(300, 300),
                         pos_hint={'center_x':0.5, 'center_y':.5})
         sv.add_widget(layout)
